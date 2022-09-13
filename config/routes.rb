b49989c6d6ae56f0ab87fa641e3030eee2ed3da1
Chris Hendricks <crshenx@gmail.com>
2022-09-12 19:23:48 -0700

    Anytthing

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       get '/rooms', to: 'rooms#index'
       post '/rooms', to: 'rooms#create'
       # I am adding this although we may not need with consumer creation
-      mount ActionCable.server => '/cable'
+      # mount ActionCable.server => '/cable'
     end
   end
 end
