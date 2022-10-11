06aad75857975a7d3e79bd11957e8867adce9024
Chris Hendricks <crshenx@gmail.com>
2022-10-11 16:40:43 -0700

    fixing avatar pic in messages

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,14 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
   # may need to add :picture symbol for response
   include Rails.application.routes.url_helpers
-  attributes :username, :avatar, :bio,
+  attributes :username, :avatar, :bio, :picture
 
-
+  
 
   def picture
     # variant = object.picture.variant(resize:"100x100")
     # return rails_representation_url(variant, only_path: true)
-    rails_blob_url(object.picture, only_path: true)
+    if object.picture
+      rails_blob_url(object.picture, only_path: true)
+    end
   end
 
 end
