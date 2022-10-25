943e51d1167cee1048096923e329870bffd156c0
Chris Hendricks <crshenx@gmail.com>
2022-10-24 17:41:48 -0700

    trying to add tokenm to params

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,6 +4,7 @@ module ApplicationCable
     
     def connect
       self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.name
     end
 
     private
@@ -15,7 +16,7 @@ module ApplicationCable
           nil
         end
 
-        if verified_user = User.find_by(id:user_id)
+        if verified_user = User.find_by(token: request.params[:token])
           verified_user
         else
           reject_unauthorized_connection
