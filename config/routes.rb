5d8c4ef885f759f8dba158ac2965833b0e5cbab7
Chris Hendricks <crshenx@gmail.com>
2022-10-25 18:20:39 -0700

    init fallback controller for signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
       mount ActionCable.server => '/cable'
     end
   end
+  get '*path',
+  to: 'fallback#index',
+  constraints: ->(req) { !req.xhr? && req.format.html? }
 end
