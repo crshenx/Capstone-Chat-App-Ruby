7a016d70df03f3b7ad9d9eaa5837337d3c777766
Logan Figgins <l.figgins.se@gmail.com>
2022-11-29 17:58:51 -0800

    containerize api service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,12 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.hosts = [
+    IPAddr.new("0.0.0.0/0"),
+    "localhost",
+    "api"
+  ]
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
